4045fe805e6c7575e019669b725a50ff05d38a5a
Fabio Soares <fabio@exodus.eti.br>
2024-02-01 17:10:48 -0300

    Removed the buttons from the index page

---
+++ js/script.js
@@ -0,0 +1,51 @@
+function removeAll() {
+    graph.dispose();
+    ctx.clearRect(0, 0, myCanvas.width, myCanvas.height);
+    graph.draw(ctx);                
+}
+function removeRandomPoint() {
+    if (graph.points.length == 0) {
+        console.log("no points");
+        return;
+    }
+    const index = Math.floor(Math.random() * graph.points.length);
+    graph.removePoint(graph.points[index]);
+    ctx.clearRect(0, 0, myCanvas.width, myCanvas.height);
+    graph.draw(ctx);
+}
+
+function removeRandomSegment() {
+    if (graph.segments.length == 0) {
+        console.log("no segments");
+        return;
+    }
+    const index = Math.floor(Math.random() * graph.segments.length);
+    graph.removeSegment(graph.segments[index]);
+    ctx.clearRect(0, 0, myCanvas.width, myCanvas.height);
+    graph.draw(ctx);
+}
+function addRandomSegment() {
+    const index1 = Math.floor(Math.random() * graph.points.length);
+    const index2 = Math.floor(Math.random() * graph.points.length);
+    const success = graph.tryAddSegment(
+            new Segment(graph.points[index1], graph.points[index2])
+        );
+    ctx.clearRect(0, 0, myCanvas.width, myCanvas.height);
+    graph.draw(ctx);
+    console.log(success);
+}
+function addRandomPoint() {
+    const success = graph.tryAddPoint(
+        new Point(
+            Math.random() * myCanvas.width,
+            Math.random() * myCanvas.height
+        )
+    );
+    ctx.clearRect(0, 0, myCanvas.width, myCanvas.height);
+    graph.draw(ctx);
+    console.log(success);
+}
+
+const controlButtons = document.querySelectorAll("#controls button");
+console.log(controlButtons);
+controlButtons.forEach(e => e.classList.add("hide"));
