4de9821020aaa3bf01f55e969f151a3d86815959
Fabio Soares <fabio@exodus.eti.br>
2024-02-07 00:34:55 -0300

    class world to handle all the elements, roads and paths

---
+++ js/world.js
@@ -0,0 +1,35 @@
+class World {
+    constructor(graph, roadWidth = 100, roadRoundness = 3) {
+        this.graph = graph;
+        this.roadWidth = roadWidth;
+        this.roadRoundness = roadRoundness;
+
+        this.envelopes = [];
+
+        this.generate();
+    }
+
+    generate() {
+        this.envelopes.length = 0;
+        for (const seg of this.graph.segments) {
+            this.envelopes.push(
+                new Envelope(seg, this.roadWidth, this.roadRoundness)
+            );
+        }
+
+        // this.intersections = Polygon.break(
+        //     this.envelopes[0].poly,
+        //     this.envelopes[1].poly
+        // );
+        Polygon.multiBreak(this.envelopes.map((e) => e.poly));
+    }
+
+    draw(ctx) {
+        for (const env of this.envelopes) {
+            env.draw(ctx);
+        }
+        // for (const int of this.intersections) {
+        //     int.draw(ctx, { color: "red", size: 6 });
+        // }        
+    }
+}
